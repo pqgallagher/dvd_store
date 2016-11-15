6aeb81731519130f68a9fc8a076e4a3104523792
pqgallagher <pqgallagher@gmail.com>
2016-11-15 11:11:07 -0600

    Created shopping Cart

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,23 @@
 class MoviesController < ApplicationController
+  before_action :initialize_session
+  before_action :load_movies_in_cart, only: [:index]
+
   def index
     @movies = Movie.order(:title)
   end
 
   def show
     @movie = find_movie
+  end
+
+  def add_to_cart
+   id = params[:id].to_i
+   session[:movies_in_cart] << id unless session[:movies_in_cart].include?(id)
+
+   redirect_to index_path
+  end
+
+  def remove_from_cart
   end
 
   private
@@ -15,4 +28,13 @@ class MoviesController < ApplicationController
   def movie_params
     params.require(:movie).permit(:title)
   end
+
+  def initialize_session
+    session[:movies_in_cart] ||= []
+  end
+
+  def load_movies_in_cart
+    @movies_in_cart = Movie.find(session[:movies_in_cart])
+  end
+
 end
