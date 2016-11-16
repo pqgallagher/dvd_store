f851137a1f2d55736b42c189b004abf1779536cd
pqgallagher <pqgallagher@gmail.com>
2016-11-16 09:21:01 -0600

    Updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,21 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  #GET / loads the index action of the tasks controller
+  # GET / loads the index action of the tasks controller
   root 'movies#index', as: 'index'
   get 'movies/:id' => 'movies#show', as: 'movie', id: /\d+/
+
+  # get 'movies/:id' => 'movies#index', as: 'sort', id: /\d+/
+  # post :sort
+
+  resources :movies, only: [:index] do
+    member do
+      post :add_to_cart
+      post :remove_from_cart
+    end
+    collection do
+      post :remove_all
+    end
+  end
+
 end
