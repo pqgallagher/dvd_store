a23f9ab5a93ec0862df05c261b84c7022651355e
pqgallagher <pqgallagher@gmail.com>
2016-12-04 22:18:55 -0600

    Updated contact.js

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,9 +1,16 @@
+//Displays the google map based on the LatLng value.
 function myMap()
 {
+  //Gets the div object to display the map.
   var mapCanvas = document.getElementById("map");
+  //Creates a new google map object,
   var myCenter = new google.maps.LatLng(49.9002628,-97.1412676);
+  //Sets the location and zoom.
   var mapOptions = {center: myCenter, zoom: 18};
+  //Sets the map buttons and display options.
   var map = new google.maps.Map(mapCanvas,mapOptions);
+  //Centers the map.
   var marker = new google.maps.Marker({position: myCenter});
+  //Sets the marker for the location.
   marker.setMap(map);
 }
